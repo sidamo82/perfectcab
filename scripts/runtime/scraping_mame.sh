e21e02edb750c287a1c0f2e45e6ee3db2ea8d2e0
Nicola Damonti <>
2020-05-28 11:46:53 +0200

    fix scripts

--- scripts/runtime/scraping_mame.sh
+++ scripts/runtime/scraping_mame.sh
@@ -23,5 +23,5 @@
 /usr/local/bin/Skyscraper -p mame-mame4all -a /usr/local/etc/skyscraper/artwork.xml -c /usr/local/etc/skyscraper/config.ini -i /media/perfectcab/roms/mame -g /media/perfectcab/skyscraper/mame -f attractmode -e /usr/local/share/attract/attract.cfg --flags unattend,skipped,nobrackets,videos
 #/bin/sed 's/attract/Mame/g' /media/perfectcab/skyscraper/mame/attract.txt > /usr/local/share/attract/romlists/Mame.txt
 /usr/local/bin/Skyscraper -p mame-mame4all -a /usr/local/etc/skyscraper/artwork.xml -c /usr/local/etc/skyscraper/config.ini -i /media/perfectcab/roms/mame -g /media/perfectcab/skyscraper/mame -f attractmode -e /usr/local/share/attract/attract.cfg --flags unattend,skipped,nobrackets,videos
-/usr/bin/php create_attract_romlist_mame.php /media/perfectcab/roms/mame /media/perfectcab/db/mame.db > /usr/local/share/attract/romlists/Mame.txt
-/usr/bin/php create_attract_romlist_neogeo.php /media/perfectcab/roms/mame /media/perfectcab/db/mame.db > /usr/local/share/attract/romlists/Neo\ Geo\ MVS.txt
+/usr/bin/php /usr/local/bin/create_attract_romlist_mame.php /media/perfectcab/roms/mame /media/perfectcab/db/mame.db > /usr/local/share/attract/romlists/Mame.txt
+/usr/bin/php /usr/local/bin/create_attract_romlist_neogeo.php /media/perfectcab/roms/mame /media/perfectcab/db/mame.db > /usr/local/share/attract/romlists/Neo\ Geo\ MVS.txt
