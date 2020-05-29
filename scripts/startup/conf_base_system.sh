a84a974d2bffa558a60588e043f680b560ca971e
Nicola Damonti <>
2020-05-29 11:45:00 +0200

    fix lrmc install

--- scripts/startup/conf_base_system.sh
+++ scripts/startup/conf_base_system.sh
@@ -136,7 +136,7 @@ echo "---- INSTALL LRMC"
 echo $PASSWORD | cd /usr/local/src/perfectcab/software
 echo $PASSWORD | sudo -S tar xzvf lrmc-0.9.2.tar.gz
 echo $PASSWORD | sudo -S cd lrmc-0.9.2/
-echo $PASSWORD | sudo -S ./configure --prefix=/usr/local
+echo $PASSWORD | sudo -S /usr/local/src/perfectcab/software/lrmc-0.9.2/configure --prefix=/usr/local
 echo $PASSWORD | sudo -S make
 echo $PASSWORD | sudo -S make install
 echo $PASSWORD | sudo -S cd ..
